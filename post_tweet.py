f342efadeac0142fca02011f8c0d80bffea56057
abdou_hl <abderrahmane.36.hellal@gmail.com>
2023-01-25 09:20:21 +0100

    init

---
+++ post_tweet.py
@@ -0,0 +1,44 @@
+
+import json
+import os
+import sys
+import random
+import tweepy
+from dotenv import load_dotenv
+from os.path import join, dirname
+from supabase import create_client, Client
+load_dotenv(join(dirname(__file__), '.env'))
+
+
+twitter_auth_keys = {
+    "consumer_key"        : os.environ.get("QUOTES_TWITER_BOT_CONSUMER_KEY"),
+    "consumer_secret"     : os.environ.get("QUOTES_TWITER_BOT_CONSUMER_SECRET"),
+    "access_token"        : os.environ.get("QUOTES_TWITER_BOT_ACCESS_TOKEN"),
+    "access_token_secret" : os.environ.get("QUOTES_TWITER_BOT_ACCESS_TOKEN_SECRET")
+}
+
+auth = tweepy.OAuthHandler(
+        twitter_auth_keys['consumer_key'],
+        twitter_auth_keys['consumer_secret']
+        )
+auth.set_access_token(
+        twitter_auth_keys['access_token'],
+        twitter_auth_keys['access_token_secret']
+        )
+api = tweepy.API(auth)
+
+
+
+
+supabase: Client = create_client(os.environ.get("SUPABASE_URL"),os.environ.get("SUPABASE_KEY"))
+quote_kkey = random.choice(range(0,200000))
+data =supabase.table("quotes").select('*').eq('lang','en').range(quote_kkey,quote_kkey+1).execute().data
+title = data[0]['text']
+author_name = data[0]['username']
+quote_key = data[0]['key']
+auth = author_name.lower()
+
+site_url = "https://www.quotesandsayings.net/quotes/"+author_name
+
+tweet = f'@{auth_username}\n\n#{auth_tag} #quotes #quotesandsayings #motivation #inspiration #sayings #quote #quoteoftheday {site_url}-{quote_key}'
+api.update_status(status=tweet)
