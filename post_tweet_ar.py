3c0cfdd98a982b2694cf89e609cd45afb2e7e4c5
abdou_hl <abderrahmane.36.hellal@gmail.com>
2023-02-20 14:26:19 +0100

    update access tokens

--- post_tweet_ar.py
+++ post_tweet_ar.py
@@ -11,8 +11,8 @@ load_dotenv(join(dirname(__file__), '.env'))
 
 
 twitter_auth_keys = {
-    "consumer_key"        : os.environ.get("QUOTES_TWITER_BOT_CONSUMER_KEY_"),
-    "consumer_secret"     : os.environ.get("QUOTES_TWITER_BOT_CONSUMER_SECRET_"),
+    "consumer_key"        : os.environ.get("QUOTES_TWITER_BOT_CONSUMER_KEY__"),
+    "consumer_secret"     : os.environ.get("QUOTES_TWITER_BOT_CONSUMER_SECRET__"),
     "access_token"        : os.environ.get("QUOTES_AR_TWITER_BOT_ACCESS_TOKEN"),
     "access_token_secret" : os.environ.get("QUOTES_AR_TWITER_BOT_ACCESS_TOKEN_SECRET")
 }
@@ -31,7 +31,7 @@ api = tweepy.API(auth)
 
 
 supabase: Client = create_client(os.environ.get("SUPABASE_URL"),os.environ.get("SUPABASE_KEY"))
-quote_kkey = random.choice(range(0,600))
+quote_kkey = random.choice(range(0,1100))
 data =supabase.table("quotes").select('*').eq('lang','ar').range(quote_kkey,quote_kkey+1).execute().data
 title = data[0]['text']
 author_name = data[0]['username']
